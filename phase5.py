58ab84e643e081942f261c37705389356319dbc0
Brunic Feyou <brunicfeyou@Brunics-MacBook-Pro.local>
2024-10-27 12:26:18 +0100

    Réalisation de la phase 5 mise en place des graphiques avec matplotlib

--- phase5.py
+++ phase5.py
@@ -5,25 +5,91 @@ import os
 import shutil
 from download_functions import download_all_categories_images
 from urllib.parse import urljoin
+import matplotlib.pyplot as plt 
+import numpy as np
 
-array_calcul = []
-total_count = 0
-counts = 0
-column_counts = {}
+category_counts = {}
+all_books_count = 0
+sum_price = {}
+category_counts_percent = {}
+middle_price = {}
 
 with open('books_categories.csv', 'r' , encoding='utf-8') as file : 
     library_books = csv.DictReader(file)
-    for column in library_books.fieldnames:
-        column_counts[column] = 0
-    
-    # Parcourt chaque ligne et chaque colonne pour vérifier la présence de 'page_url'
-    for book in library_books:
-        for column, value in book.items():
-            if value == 'page_url':  # Vérifie si la valeur correspond exactement à 'page_url'
-                column_counts[column] += 1
 
-# Affiche les résultats
-for column, count in column_counts.items():
-    print(f"'{column}' contient '{count}' occurrences de 'page_url'")
+    # /////////////////////////////////////////// Graphique circulaire //////////////////////////////////
+    for books in library_books : 
+        category = books['category']
+        # print(category)
+        price = books["price_excluding_tax"]
+        all_books_count +=1 
+        if category in category_counts :
+            category_counts[category] +=1     
+        else : 
+            category_counts[category] = 1 
+            # print(category_counts)
+
+        # /////////////////////////////////////////// Graphique en barre //////////////////////////////////
+        price_value = float(price.replace('£', ''))
+        if category in sum_price :   
+            # print(f"le sum_price = {sum_price[category]} et la catégorie = {category_counts[category]}") 
+            sum_price[category] += price_value
+        else : 
+            sum_price[category] = price_value     
+
+
+    # ////////////////////////////////// calcul su pourcentage de livre et de la moyenne des prix /////////////////////////
+    for category, count in category_counts.items() :
+        category_counts_percent[category] = (count*100)/all_books_count
+        for category_price, price in sum_price.items() : 
+            if category_price == category : 
+                middle_price[category_price] = round(price/count, 2)
+    print(middle_price)
+
+    # ////////////////////////////////// Affichage du diagramme circulaire ///////////////////////////////
+    explode = [.2 if category == 'Womens Fiction' else .1 for category in category_counts_percent.keys()]
+    print(category_counts_percent)
+    plt.title("Le pourcentage de livres par catégories")
+    plt.pie(
+        category_counts_percent.values(), 
+        labels = category_counts_percent.keys(),
+        autopct="%.2f%%", 
+        explode=explode,
+        # shadow=True,
+        startangle=90     
+    )
+    plt.axis("equal")
+    graphique1 = "./graphiques_images/graphique_circulaire.png" 
+    plt.savefig(graphique1)
+    plt.close()
+
+    # ////////////////////////////////////////// Affichache du diagramme en barre ////////////////////////
+
+    plt.title('Le prix moyen des livres par catégorie')
+    width = .5
+    plt.bar(
+        x = middle_price.keys(),
+        height = middle_price.values(), 
+        width= width, 
+        color = 'pink'  
+    ) 
+    positions = np.arange(len(middle_price))
+    plt.xticks(positions, list(middle_price.keys()), rotation=90)
+    plt.xlim( - 0.5, len(middle_price) -0.5) 
+    plt.xlabel('Catégories')
+    plt.ylabel('Prix moyens')
+    plt.axis("equal")
+    plt.tight_layout()
+    graphique2 = "./graphiques_images/graphique_barre.png" 
+    plt.savefig(graphique2)
+    plt.close()
+
+
+
+
+  
+
+
+
 
 
